df5b52e410416764739cb7dd25aee8836124670d
Lucaasgl <lucasmatheusglowienka4@gmail.com>
2023-03-06 21:43:31 -0300

    attt

--- imc.py
+++ imc.py
@@ -1,6 +1,18 @@
+#Calculadora IMC
 peso = float(input("Digite o seu peso em quilogramas: "))
 altura = float(input("Digite a sua altura em metros: "))
 
 imc = peso / altura ** 2
 
 print("Seu IMC é:", imc)
+
+
+#Maior Valor
+
+valor1 = float(input("Digite o primeiro valor: "))
+valor2 = float(input("Digite o segundo valor: "))
+
+if valor1 > valor2:
+    print("O maior valor é:", valor1)
+else:
+    print("O maior valor é:", valor2)
